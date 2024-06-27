d3086e30d1105411cef629ee25d248375c9a7a34
jahongir-sobirjonov <sobirjonovjahongir133@gmail.com>
2024-06-27 15:46:18 +0500

    s

---
+++ src/main/java/uniqueproject/uz/go2uzproject/Go2uzProjectApplication.java
@@ -0,0 +1,13 @@
+package uniqueproject.uz.go2uzproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Go2uzProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Go2uzProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uniqueproject/uz/go2uzproject/Go2uzProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package uniqueproject.uz.go2uzproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Go2uzProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
