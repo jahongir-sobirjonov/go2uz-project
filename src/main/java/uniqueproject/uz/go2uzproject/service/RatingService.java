f48fe4afeb383b91916d7ed3fa9ff8f2b2eb6e43
jahongir-sobirjonov <sobirjonovjahongir133@gmail.com>
2024-07-01 16:55:09 +0500

    updated TourCategory

--- src/main/java/uniqueproject/uz/go2uzproject/service/RatingService.java
+++ src/main/java/uniqueproject/uz/go2uzproject/service/RatingService.java
@@ -117,12 +117,5 @@ public class RatingService {
                 .build();
     }
 
-    public boolean threeConsecutiveOdds(int[] arr) {
-        for (int i = 0; i < arr.length - 1; i++) {
-            if (arr[i] % 2 == 0 && arr[i + 1] % 2 == 0 && arr[i + 2] % 2 == 0) {
-                return true;
-            }
-        }
-        return false;
-    }
+
 }
