ff183a51109d11b79ed1416d03186d53f139f2e1
jahongir-sobirjonov <sobirjonovjahongir133@gmail.com>
2024-07-01 16:32:57 +0500

    updated RatingService

--- src/main/java/uniqueproject/uz/go2uzproject/service/RatingService.java
+++ src/main/java/uniqueproject/uz/go2uzproject/service/RatingService.java
@@ -116,4 +116,13 @@ public class RatingService {
                 .rating(rating.getRating())
                 .build();
     }
+
+    public boolean threeConsecutiveOdds(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] % 2 == 0 && arr[i + 1] % 2 == 0 && arr[i + 2] % 2 == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
