d95639419040979ba7a924624beaa8598bdcd14c
jahongir-sobirjonov <sobirjonovjahongir133@gmail.com>
2024-07-08 13:36:11 +0500

    updated

--- src/main/java/uniqueproject/uz/go2uzproject/entity/enums/TourCategory.java
+++ src/main/java/uniqueproject/uz/go2uzproject/entity/enums/TourCategory.java
@@ -1,6 +1,6 @@
 package uniqueproject.uz.go2uzproject.entity.enums;
 
 public enum TourCategory {
-    MOUNTAIN, ENTERTAINMENT, WATERFALLS, HISTORICAL_PLACES, BEACH,
+    MOUNTAIN, ENTERTAINMENT, WATERFALLS, BEACH,
     HISTORY, DESERT, CULTURE, CITY, SAFARI, COUNTRY, FOREST// ... davomi bor yana
 }
